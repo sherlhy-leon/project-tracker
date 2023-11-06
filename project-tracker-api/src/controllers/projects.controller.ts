877bac4f5f9fc1357c31e2e9ec957d56f05d80a2
sherlhy-leon <sherlhy95@gmail.com>
2023-11-06 18:33:44 +0100

    Install dependencies and logging

--- project-tracker-api/src/controllers/projects.controller.ts
+++ project-tracker-api/src/controllers/projects.controller.ts
@@ -2,11 +2,13 @@ import { Request, Response } from 'express';
 import * as  ProjectService from '../services/projects.service';
 
 export const getProjects = async (req: Request, res: Response): Promise<void> => {
+    console.log("getProjects endpoint invoked")
     const data = await ProjectService.getProjects();
     res.status(200).json({ projects: data });
 };
 
 export const completeProject = async (req: Request, res: Response): Promise<void> => {
+    console.log("completeProject endpoint invoked")
     const projectIds = req.body;
     const data = await ProjectService.completeProject(projectIds);
     res.status(201).json({ projects: data });
