35f96c14cb331862f61121964f2bf58f1a59a08d
sherlhy-leon <sherlhy95@gmail.com>
2023-11-07 01:13:40 +0100

    Fix errors

--- project-tracker-ui/src/app/page/project-tracker/project-tracker.component.ts
+++ project-tracker-ui/src/app/page/project-tracker/project-tracker.component.ts
@@ -1,5 +1,6 @@
-import { Component, NgZone } from '@angular/core';
-import { NAV_LINKS } from './project-tracker.constants';
+import { Component } from '@angular/core';
+import { ProjectFacade } from 'src/app/facade/projects.facade';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-project-tracker',
@@ -7,9 +8,14 @@ import { NAV_LINKS } from './project-tracker.constants';
   styleUrls: ['./project-tracker.component.scss']
 })
 export class ProjectTrackerComponent {
-  projectsTabs: {url: string; label: string;}[];
-  constructor(private readonly zone: NgZone) {
-    this.projectsTabs = NAV_LINKS
-    // this.zone.run(()=>{})
+  projectsTabs: { url: string; label: string; count: Observable<number> }[];
+
+  constructor(private projectFacade: ProjectFacade) {
+    this.projectFacade.loadProjects();
+    this.projectsTabs = [
+      { url: '/projects/all', label: 'ALL', count: this.projectFacade.projects$.pipe(map(projects => projects.length)) },
+      { url: '/projects/pending', label: 'PENDING', count: this.projectFacade.pendingProjects$.pipe(map(projects => projects.length)) },
+      { url: '/projects/completed', label: 'COMPLETED', count: this.projectFacade.completedProjects$.pipe(map(projects => projects.length)) }
+    ]
   }
 }
